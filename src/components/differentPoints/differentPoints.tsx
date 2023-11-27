4a968f35c42b816ba335d620aea9d99ce5853b59
Igor Shadurin <igor.shadurin@gmail.com>
2023-11-27 14:59:24 +0300

    refactor: grey bottom

--- src/components/differentPoints/differentPoints.tsx
+++ src/components/differentPoints/differentPoints.tsx
@@ -64,98 +64,88 @@ function DifferentPoints(props: Props) {
                   <strong>Expanding dApp Store:</strong>{' '}
                   Explore and use a diverse range of dApps, all while maintaining sovereignty over your data.
                 </div>
+                <div className="margin-top bottom-form">
+                  <Typography variant="h2" gutterBottom={true} className="headers">
+                    <b>
+                      Reserve Your Spot as an Early Adopter
+                    </b>
+                  </Typography>
 
-                <Typography variant="h2" gutterBottom={true} className="headers margin-top">
-                  <b>
-                    Reserve Your Spot as an Early Adopter
-                  </b>
-                </Typography>
+                  <Typography variant="h3" gutterBottom={true} className="headers">
+                    <b>
+                      Join the waiting list
+                    </b>
+                  </Typography>
 
-                {/*<div className={classes.headerSmall} style={{marginBottom: '4rem'}}>*/}
-                {/*  Join the waiting list*/}
-                {/*</div>*/}
-
-                <Typography variant="h3" gutterBottom={true} className="headers">
-                  <b>
-                    Join the waiting list
-                  </b>
-                </Typography>
-
-                <div className={classes.p1} style={{
-                  marginTop: '2rem',
-                  marginRight: '0',
-                  // display: 'flex',
-                  // flexDirection: 'column',
-                  // alignItems: 'center'
-                }}>
-                  <p style={{
-                    // color: '#888',
-                    // opacity: 0.6,
-                    // fontSize: '1.8rem',
-                    // textAlign: 'center',
-                    backgroundColor: '#f5f5f5',
-                    padding: '2rem',
+                  <div className={classes.p1} style={{
+                    marginTop: '2rem',
+                    marginRight: '0',
                   }}>
-                    By entering the waiting list for Fairdrive, you're not just early—you're exclusive. As an early
-                    adopter, you'll secure privileged access to a platform that’s setting the new standard for data
-                    autonomy. Any future benefits that come with early adoption are just the icing on the cake.
-                  </p>
-                  {/*<div className={classes.newsletterBar}>*/}
-                  {/*  <input*/}
-                  {/*    className={classes.newsletterBarInput}*/}
-                  {/*    type="text"*/}
-                  {/*    placeholder="Enter your email"*/}
-                  {/*  />*/}
-                  {/*  <button type="button" className={classes.newsletterBarSubmit}>*/}
-                  {/*    GET EARLY ACCESS*/}
-                  {/*  </button>*/}
-                  {/*</div>*/}
+                    <p style={{
+                      backgroundColor: '#f5f5f5',
+                      // padding: '2rem',
+                    }}>
+                      By entering the waiting list for Fairdrive, you're not just early—you're exclusive. As an early
+                      adopter, you'll secure privileged access to a platform that’s setting the new standard for data
+                      autonomy. Any future benefits that come with early adoption are just the icing on the cake.
+                    </p>
+                    {/*<div className={classes.newsletterBar}>*/}
+                    {/*  <input*/}
+                    {/*    className={classes.newsletterBarInput}*/}
+                    {/*    type="text"*/}
+                    {/*    placeholder="Enter your email"*/}
+                    {/*  />*/}
+                    {/*  <button type="button" className={classes.newsletterBarSubmit}>*/}
+                    {/*    GET EARLY ACCESS*/}
+                    {/*  </button>*/}
+                    {/*</div>*/}
 
-                  {showThanks && <div>
-                      <div id="sign-result">
-                          THANKS FOR SIGNING UP!
-                      </div>
-                      <p className="text-center">
-                          Please check your email to confirm it's really you.
-                      </p>
-                  </div>}
+                    {showThanks && <div>
+                        <div id="sign-result">
+                            THANKS FOR SIGNING UP!
+                        </div>
+                        <p className="text-center">
+                            Please check your email to confirm it's really you.
+                        </p>
+                    </div>}
 
-                  {!showThanks && <div id="mauticform_wrapper_fairdrivelaunch" className="form-container">
-                      <form autoComplete="false" role="form" method="post"
-                            action="https://mt.fairdrive.io/form/submit?formId=1" id="mauticform_fairdrivelaunch"
-                            data-mautic-form="fairdrivelaunch" encType="multipart/form-data" onSubmit={() => {
-                        setShowThanks(true)
-                      }}>
-                          <div className="mauticform-error" id="mauticform_fairdrivelaunch_error"></div>
-                          <div className="mauticform-message" id="mauticform_fairdrivelaunch_message"></div>
-                          <div className="mauticform-innerform">
+                    {!showThanks && <div id="mauticform_wrapper_fairdrivelaunch" className="form-container">
+                        <form autoComplete="false" role="form" method="post"
+                              action="https://mt.fairdrive.io/form/submit?formId=1" id="mauticform_fairdrivelaunch"
+                              data-mautic-form="fairdrivelaunch" encType="multipart/form-data" onSubmit={() => {
+                          setShowThanks(true)
+                        }}>
+                            <div className="mauticform-error" id="mauticform_fairdrivelaunch_error"></div>
+                            <div className="mauticform-message" id="mauticform_fairdrivelaunch_message"></div>
+                            <div className="mauticform-innerform">
 
-                              <div data-mautic-form-page="1">
-                                  <div id="mauticform_fairdrivelaunch_email" className="mauticform-row">
-                                      <input id="mauticform_input_fairdrivelaunch_email" name="mauticform[email]"
-                                             placeholder="Email" className="email-input" type="email"/>
-                                      <span className="mauticform-errormsg" style={{display: 'none'}}></span>
-                                  </div>
+                                <div data-mautic-form-page="1">
+                                    <div id="mauticform_fairdrivelaunch_email" className="mauticform-row">
+                                        <input id="mauticform_input_fairdrivelaunch_email" name="mauticform[email]"
+                                               placeholder="Email" className="email-input" type="email"/>
+                                        <span className="mauticform-errormsg" style={{display: 'none'}}></span>
+                                    </div>
 
-                                  <div id="mauticform_fairdrivelaunch_subscribe" className="mauticform-row">
-                                      <button type="submit" name="mauticform[subscribe]"
-                                              id="mauticform_input_fairdrivelaunch_subscribe"
-                                              className="submit-button">Join Waiting List
-                                      </button>
-                                  </div>
-                              </div>
-                          </div>
+                                    <div id="mauticform_fairdrivelaunch_subscribe" className="mauticform-row">
+                                        <button type="submit" name="mauticform[subscribe]"
+                                                id="mauticform_input_fairdrivelaunch_subscribe"
+                                                className="submit-button">Join Waiting List
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
 
-                          <input type="hidden" name="mauticform[formId]" id="mauticform_fairdrivelaunch_id" value="1"/>
-                          <input type="hidden" name="mauticform[return]" id="mauticform_fairdrivelaunch_return"
-                                 value=""/>
-                          <input type="hidden" name="mauticform[formName]" id="mauticform_fairdrivelaunch_name"
-                                 value="fairdrivelaunch"/>
-                      </form>
-                  </div>}
+                            <input type="hidden" name="mauticform[formId]" id="mauticform_fairdrivelaunch_id"
+                                   value="1"/>
+                            <input type="hidden" name="mauticform[return]" id="mauticform_fairdrivelaunch_return"
+                                   value=""/>
+                            <input type="hidden" name="mauticform[formName]" id="mauticform_fairdrivelaunch_name"
+                                   value="fairdrivelaunch"/>
+                        </form>
+                    </div>}
 
+                  </div>
                 </div>
-
               </div>
             </div>
           </div>
